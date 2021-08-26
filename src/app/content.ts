ae29f99884c448cccf746a99f07a026ceb6e9972
Rani <rhorev@netflix.com>
2021-08-25 17:09:48 -0700

    added retries

--- src/app/content.ts
+++ src/app/content.ts
@@ -1,20 +1,36 @@
 chrome.runtime.sendMessage({}, (response) => {
-  var checkReady = setInterval(() => {
+  let checkReady = setInterval(() => {
     if (document.readyState === "complete") {
       clearInterval(checkReady);
-      runScript();
+      retry(10, runScript);
     }
-  });
+  }, 100);
 });
 
+const retry = (maxRetries: number, fn: () => void) => {
+  console.log(`Running, attempts left: ${maxRetries}`);
+  try {
+    fn();
+  } catch (e) {
+    if (maxRetries <= 0) {
+      throw e;
+    }
+    setTimeout(() => retry(maxRetries - 1, fn), 500);
+  }
+};
+
 const runScript = () => {
-  console.log("init");
-  const betterHeader = document.querySelector(
-    ".pull-request-header h2"
-  ).innerHTML;
+  const betterHeaderElem = document.querySelector(".pull-request-header h2");
+
+  if (!betterHeaderElem) {
+    throw new Error("Better header elem is missing");
+  }
+
   const elem = document.querySelector("#commit-message");
-  console.log(elem);
   if (elem) {
-    elem.innerHTML = betterHeader;
+    elem.innerHTML = betterHeaderElem.innerHTML;
+    elem.setAttribute("data-original-value", betterHeaderElem.innerHTML);
+  } else {
+    throw new Error("commit message elem was not found");
   }
 };
